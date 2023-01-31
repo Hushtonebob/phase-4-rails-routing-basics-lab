ec8db74773c6dbe5d37b0a83152a2dd7fbc76aff
Bobby De Luna <bobby.deluna2@gmail.com>
2023-01-31 00:19:02 -0700

    commit tree e234f76ac3c96f07ab3a445b1b863d1806d7e364

---
+++ app/controllers/students_controller.rb
@@ -0,0 +1,14 @@
+class StudentsController < ApplicationController
+
+    def index
+        students = Student.all
+        render json: students
+    end
+
+    def grades
+        blam = Student.all
+        grades =grades.order(:grade)
+        render json: grades
+    end
+
+end
